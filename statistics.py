2624bf116a383e57e74745545618dd905256d0e5
gchhughes <74072434+gchhughes@users.noreply.github.com>
2022-03-18 21:08:26 -0700

    Created empty df for graded ducts

--- statistics.py
+++ statistics.py
@@ -1,8 +1,8 @@
-#%% Modules
+# %% Modules
 import pandas as pd
 import matplotlib as plt
 import numpy as np
-#%% Obtain paths to statistics
+# %% Obtain paths to statistics
 pathDir = r'C:\Users\griff\Box\CASIT\Files for Derrick\duct-analysis'
 trackerDir = pathDir + '\Tracker.xlsx'
 
@@ -23,8 +23,17 @@ for i in range(cases.shape[0]):
     cases.iloc[i,0] = cases.iloc[i,0].zfill(3)
 
     cases.iloc[i,1] = ('{}\\{}\\results_{}\\results_{}.xlsx'.format(pathDir,cases.iloc[i,0],cases.iloc[i,0],cases.iloc[i,0]))
-#%% Import all data into dataframe
+# %% Import all data into dataframe
 cases = cases.assign(Area = np.nan,EquivDiam = np.nan, MajorAxisLength = np.nan, Cancer = np.nan, Slice = np.nan)
 #for i in range(cases.shape[0]):
 
-# %%
+# %% Create separate df for each Gleason score
+healthy = pd.DataFrame(columns = ['Area','EquivDiam','MajorAxisLength'])
+g33 = pd.DataFrame(columns = ['Area','EquivDiam','MajorAxisLength'])
+g34 = pd.DataFrame(columns = ['Area','EquivDiam','MajorAxisLength'])
+g43 = pd.DataFrame(columns = ['Area','EquivDiam','MajorAxisLength'])
+g44 = pd.DataFrame(columns = ['Area','EquivDiam','MajorAxisLength'])
+g45 = pd.DataFrame(columns = ['Area','EquivDiam','MajorAxisLength'])
+g55 = pd.DataFrame(columns = ['Area','EquivDiam','MajorAxisLength'])
+
+# %% Plot Histograms
