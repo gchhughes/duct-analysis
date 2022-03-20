6f27d158d6ed5a0899e5f78a3b628efc665312f0
gchhughes <74072434+gchhughes@users.noreply.github.com>
2022-03-19 21:35:58 -0700

    Gleason score histogram not working properly

--- statistics.py
+++ statistics.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import ks_2samp, zscore
 
 # %% Obtain paths to statistics
 pathDir = r'C:\Users\griff\Box\CASIT\Files for Derrick\duct-analysis'
@@ -32,7 +33,9 @@ g34 = np.zeros((1,4))
 g43 = np.zeros((1,4))
 g44 = np.zeros((1,4))
 g45 = np.zeros((1,4))
+g54 = np.zeros((1,4))
 g55 = np.zeros((1,4))
+totCancer = np.zeros((1,4))
 
 # %% Import data and append it to arrays
 max = np.zeros((cases.shape[0],3)) # Area, Equivalent Diamater, Major Axis Length
@@ -48,27 +51,67 @@ for i in range(cases.shape[0]):
         if temp[i,3] == 0:
             healthy = np.vstack((healthy,temp[i,:]))
         elif temp[i,3] == 33:
-            g33 = np.vstack((healthy,temp[i,:]))
+            g33 = np.vstack((g33,temp[i,:]))
+            totCancer = np.vstack((totCancer,temp[i,:]))
         elif temp[i,3] == 34:
-            g34 = np.vstack((healthy,temp[i,:]))
+            g34 = np.vstack((g34,temp[i,:]))
+            totCancer = np.vstack((totCancer,temp[i,:]))
         elif temp[i,3] == 44:
-            g44 = np.vstack((healthy,temp[i,:]))
+            g44 = np.vstack((g44,temp[i,:]))
+            totCancer = np.vstack((totCancer,temp[i,:]))
         elif temp[i,3] == 45:
-            g45 = np.vstack((healthy,temp[i,:]))
+            g45 = np.vstack((g45,temp[i,:]))
+            totCancer = np.vstack((totCancer,temp[i,:]))
+        elif temp[i,3] == 54:
+            g54 = np.vstack((g54,temp[i,:]))
+            totCancer = np.vstack((totCancer,temp[i,:]))
         elif temp[i,3] == 55:
-            g55 = np.vstack((healthy,temp[i,:]))
+            g55 = np.vstack((g55,temp[i,:]))
+            totCancer = np.vstack((totCancer,temp[i,:]))
+
+# %% Check arrays
+print(g33)
+print(g34)
+print(g43)
+print(g44)
+print(g45)
+print(g54)
+print(g55)
+
+# %% Remove first row
+healthy = healthy[1:healthy.shape[0],:]
+g33 = g33[1:g33.shape[0],:]
+g34 = g34[1:g34.shape[0],:]
+# g43 = g43[1:g43.shape[0],:]
+g44 = g44[1:g44.shape[0],:]
+# g45 = g45[1:g45.shape[0],:]
+# g54 = g54[1:g54.shape[0],:]
+# g55 = g55[1:g55.shape[0],:]
+totCancer = healthy[1:healthy.shape[0],:]
+
+print(g33.shape[0]+g34.shape[0]+g44.shape[0])
+print(totCancer.shape[0])
 
 # %% Plot Histograms
+i = 0 # 0: Area; 1: Equivalent Diameter; 2: Major Axis Length
 fig, ax = plt.subplots(2, 1, sharex=True)
-cancer = [g33[1:g33.shape[0],0],g34[1:g34.shape[0],0],g44[1:g44.shape[0],0],g45[1:g45.shape[0],0]]
+plt1 = [healthy[:,i],totCancer[:,i]]
+# plt2 = [g33[1:g33.shape[0],i],g34[1:g34.shape[0],i],g44[1:g44.shape[0],i],g45[1:g45.shape[0],i],g55[1:g55.shape[0],i]]
+plt2 = [g33[:,i],g34[:,i],g44[:,i]]
 # bins = np.linspace(0,np.max(max[:,0]),1000)
 bins = np.linspace(0,0.125,1000)
+# colors = ['b','g','y','orange','red','magenta','purple']
+# labels = ['3+3','3+4','4+3','4+4','4+5','5+4','5+5']
+colors = ['b','g','orange']
+labels = ['3+3','3+4','4+4']
 
-colors = ['b','g','y','orange']
-labels = ['3+3','3+4','4+4','4+5']
-ax[0].hist(healthy[1:healthy.shape[0],0],bins=bins,label='Healthy')
+ax[0].hist(plt1,bins=bins,color=['b','r'],label=['Healthy','Total Cancer'])
 ax[0].legend()
-ax[1].hist(cancer,bins=bins,color=colors,label=labels)
-ax[1].legend()
+ax[1].hist([g33[:,i],g34[:,i],g44[:,i]],bins=bins,color=colors,label=labels)
+ax[1].legend(title='Gleason Grade')
 fig.show()
-# %%
+
+# %% Convert Histogram to CDF
+print(len(plt2))
+
+# %% KS Test
